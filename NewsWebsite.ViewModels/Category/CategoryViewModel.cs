36e9ce561141464b3884ada52e71a3e696744a8b
keyvan-alishiri <66223520+keyvan-alishiri@users.noreply.github.com>
2021-03-10 21:38:34 +0330

    Project Layering and Add AdminPanelLayout ,CategoryManager
    Add AutoMapper , DeleteGroup , UrlValidation

--- NewsWebsite.ViewModels/Category/CategoryViewModel.cs
+++ NewsWebsite.ViewModels/Category/CategoryViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using NewsWebsite.Common.Attributes;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -25,6 +26,7 @@ namespace NewsWebsite.ViewModels.Category
         public string ParentCategoryId { get; set; }
         [Display(Name = "آدرس"),JsonProperty("آدرس")]
         [Required(ErrorMessage = "وارد نمودن {0} الزامی است.")]
+        [UrlValidate("/",@"\"," ")]
         public string Url { get; set; }
     }
 }
